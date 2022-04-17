cd6504d6efb740693e819ef73dc4e2b95c69960e
Alan Diaz <alandc1400@gmail.com>
2022-04-16 20:59:30 -0500

    css pc

--- tienda-pr/src/views/navbar.js
+++ tienda-pr/src/views/navbar.js
@@ -160,6 +160,102 @@ const Navbar = () => {
           </p>
         </div>
       </div>
+      <div className="navPC nav">
+        <a href="/">
+          <img className="navLogo" src={logo} />
+        </a>
+        <input type="text" className="searchInput" placeholder="Buscar" />
+        <a className="lupa">
+          <FiSearch />
+        </a>
+        <div className="navSub">
+          <a
+            ref={dropdownRef}
+            className={`navSubMenu ${isActive ? "inactive" : "active"}`}
+            onClick={() => subMenu()}
+          >
+            <FiMenu />
+          </a>
+          <a
+            ref={dropdownRef}
+            className={`navSubMenu ${isActive ? "active" : "inactive"}`}
+            onClick={subMenu}
+          >
+            <FiX />
+          </a>
+        </div>
+
+        <div
+          ref={dropdownRef}
+          className={`subMenuLogos ${isActive ? "active" : "inactive"}`}
+        ></div>
+
+        <div
+          ref={dropdownRef}
+          className={`subMenu ${isActive ? "active" : "inactive"}`}
+        >
+          <div className="op">
+            <a className="opcion" href="/">
+              Inicio
+            </a>
+            <a className="opcionLogos" href="/">
+              <FiHome />
+            </a>
+          </div>
+          <div className="op">
+            <a className="opcion" href="/Tienda">
+              Tienda
+            </a>
+            <a className="opcionLogos" href="/Tienda">
+              <FiShoppingBag />
+            </a>
+          </div>
+          <div className="op">
+            <a className="opcion" href="/SobreNosotros">
+              Nosotros
+            </a>
+            <a className="opcionLogos" href="/SobreNosotros">
+              <FiInfo />
+            </a>
+          </div>
+          <div className="op">
+            <a className="opcion" href="/Contacto">
+              Contacto
+            </a>
+            <a className="opcionLogos" href="/Contacto">
+              <FiMail />
+            </a>
+          </div>
+          <div className="op">
+            {/* <Login /> */}
+            <a className="opcion" href="/SignUp">
+              Login
+            </a>
+            {/* <Login /> */}
+            <a className="opcionLogos" href="/SignUp">
+              <FiUser />
+            </a>
+          </div>
+          <div className="op">
+            {/*Borrar tras la revisión al publico*/}
+            <a className="opcion" href="/MiCuenta">
+              Mi Cuenta
+            </a>
+            {/*Borrar tras la revisión al publico*/}
+            <a className="opcionLogos" href="/MiCuenta">
+              <FiUser />
+            </a>
+          </div>
+          <div className="op">
+            <a className="opcion" href="/MiCarrito">
+              Mi carrito
+            </a>
+            <a className="opcionLogos" href="/MiCarrito">
+              <FiShoppingCart />
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
