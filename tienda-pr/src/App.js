fb29e8afbcef5f278cff4953d6470bb363531709
Alan Diaz <alandc1400@gmail.com>
2022-06-21 10:39:28 -0500

    mena

--- tienda-pr/src/App.js
+++ tienda-pr/src/App.js
@@ -10,7 +10,7 @@ import SignUp from "./views/SignUp";
 import Carrito from "./views/Carrito";
 import LogIn from "./views/LogIn";
 // import MiCuenta from "./views/MiCuenta";
-// import Producto from "./views/Producto";
+import Producto from "./views/Producto";
 // import Footer from "./views/Footer";
 // import PuenteCuenta from "./views/PuenteCuenta";
 // import PuenteCarrito from "./views/PuenteCarrito";
@@ -32,6 +32,7 @@ const App = () => {
           <Route path="/MiCuenta" element={<PuenteCuenta />} />
           <Route path="/Producto/:idProd" element={<Producto />} /> 
         */}
+          <Route path="/Producto" element={<Producto />} />
           <Route path="/MiCarrito" element={<Carrito />} />
           <Route path="/Envio" element={<Envio />} />
           <Route path="/Tienda" element={<Tienda />} />
--- tienda-pr/src/views/Producto.js
+++ tienda-pr/src/views/Producto.js
@@ -1,8 +1,14 @@
+// https://react-icons.github.io/react-icons/icons?name=gi
+//https://www.bareperformancenutrition.com/products/vegan-protein
+
 import React, { useState, useRef } from "react";
-import "../cssVieja/producto.css";
+import "../css/producto.css";
 
 import prod2 from "../multimedia/prod2.JPG";
 import prod1 from "../multimedia/prod1.JPG";
+import logo from "../multimedia/Logo.png";
+import { BsFacebook } from "react-icons/bs";
+import { BsInstagram } from "react-icons/bs";
 
 import { BsFillPlusCircleFill } from "react-icons/bs";
 import { BsFillDashCircleFill } from "react-icons/bs";
@@ -42,36 +48,7 @@ const Producto = () => {
     }
   };
 
-  return (
-    <div>
-      <div className="Producto">
-        <ObtenerProd
-          setData={setData}
-          data={data}
-          setFlag={setFlag}
-          id={idProd}
-        />
-        {Img(data.producto0.nombre)}
-        <h1 className="tituloProdInd">{data.producto0.nombre}</h1>
-        <h2 className="precioProdInd">$ {data.producto0.precio}</h2>
-        <p className="cuerpoTexto">{data.producto0.descripcion}</p>
-        <div className="cantidadProdInd">
-          <span className="cantProdInd">Cantidad: </span>
-          <a className="btnCantProdInd" onClick={() => Disminuir()}>
-            <BsFillDashCircleFill />
-          </a>
-          <span className="cantAgregar">{cantidad}</span>
-          {/* <input className="cantAgregar" type="text" value={cantidad} onChange={(e) => setCantidad(e)} /> */}
-          <a className="btnCantProdInd" onClick={() => Aumentar()}>
-            <BsFillPlusCircleFill />
-          </a>
-        </div>
-        <button className="agregarCarro">
-          <span className="textoAgregarCarro">Agregar al carrito</span>
-        </button>
-      </div>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Producto;
