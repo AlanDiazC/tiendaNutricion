1678808fede67572e4f5644a0a26f7f94f1eb2b5
Alan Diaz <alandc1400@gmail.com>
2022-06-22 12:09:20 -0500

    Revert "Revert "name change""
    This reverts commit 9fb48923752fcf6515420d278fc919868ba5e675.

--- tienda-pr/src/views/Producto.js
+++ tienda-pr/src/views/Producto.js
@@ -48,7 +48,119 @@ const Producto = () => {
     }
   };
 
-  return <div></div>;
+
+  return <div>
+    <div className="seccion_arriba_producto">
+      <div className="container_producto">
+        <div className="producto_fila">
+          <div className="lado_izquierdo_prod">
+            <div className="logo_producto">
+              <img src={logo}></img>
+            </div>
+            <div className="texto_del_logo">
+              <h2>Texto del texto xd</h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Turpis egestas sed tempus urna et. Nunc sed augue lacus viverra vitae congue eu.</p>
+            </div>
+          </div>
+          <div className="parte_mitad_prod">
+            <div className="slide_de_imagenes">
+              <div className="anterior_prod">
+                <button>anterior</button>
+              </div>
+              <div className="imagenes">
+                <img src={prod1}></img>
+              </div>
+              <div className="siguiente_prod">
+                <button>siguiente</button>
+              </div>
+            </div>
+          </div>
+          <div className="lado_derecho_prod">
+            <div className="lado_derecho_prod_texto">
+              <h1>Nombre del producto</h1>
+              <h3>Precio</h3>
+            </div>
+            <div className="seleccion_producto">
+              <div className="cantidad_de_productos">
+                <select>Cantidad</select>
+              </div>
+              <div className="tipo_de_producto">
+                <select>Tipo</select>
+              </div>
+              <div>
+                <button>Agregar al carrito</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="custom_tab_pequenio">
+      <div className="container_producto">
+        <ul className="navegacion_tab">
+          <li className="nav_item">
+            <a className="nav_enlace">Beneficios</a>
+          </li>
+          <li className="nav_item">
+            <a className="nav_enlace">Nutricion</a>
+          </li>
+          <li className="nav_item">
+            <a className="nav_enlace">Componentes principales</a>
+          </li>
+          <li className="nav_item">
+            <a className="nav_enlace">Instrucciones de uso</a>
+          </li>
+        </ul>
+        <div className="contenido_tab">
+          <div className="tab_panel">
+            <div className="beneficios_primarios">
+              <div className="fila_beneficios">
+                <div className="columna_beneficios">
+                  <div className="imagen_beneficios">
+                    <img></img>
+                  </div>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore</p>
+                </div>
+                <div className="columna_beneficios">
+                  <div className="imagen_beneficios">
+                    <img></img>
+                  </div>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore</p>
+                </div>
+                <div className="columna_beneficios">
+                  <div className="imagen_beneficios">
+                    <img></img>
+                  </div>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore</p>
+                </div>
+                <div className="columna_beneficios">
+                  <div className="imagen_beneficios">
+                    <img></img>
+                  </div>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore</p>
+                </div>
+                <div className="columna_beneficios">
+                  <div className="imagen_beneficios">
+                    <img></img>
+                  </div>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="tab_panel">
+
+          </div>
+          <div className="tab_panel">
+
+          </div>
+          <div className="tab_panel">
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 };
 
 export default Producto;
