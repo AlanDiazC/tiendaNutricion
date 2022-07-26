b379e1c429da3c3b22e31e6134656c07a823b970
dian0312 <diegomeniussss1sec@gmail.com>
2022-07-26 14:54:01 -0500

    easypos

--- src/easypost.js
+++ src/easypost.js
@@ -4,6 +4,16 @@ const Easypost = require('@easypost/api');
 const api = new Easypost('EZTK7747c4d7bb5046479cd237ee70edcd6d3Kl6842VvS3GfP9rsEdS7g');
 
 const fromAddress = new api.Address({
+  /*company: 'Runbed',
+  street1: 'CHIMALPOPOCA 294, TRANSITO, 06820',
+  street2: 'D 201',
+  city: 'Mexico City',
+  state: 'Cuauhtemoc',
+  //Colonia: 'Col. Transito',
+  country: 'MX',
+  zip: '06820',
+  phone: '5561412379',
+  email: 'runbed@jotos.com'*/
   company: 'EasyPost',
   street1: '417 Montgomery Street',
   street2: '5th Floor',
@@ -13,9 +23,20 @@ const fromAddress = new api.Address({
   phone: '415-528-7555'
 });
 
-fromAddress.save().then(console.log);
+fromAddress.save().then(console.log("DIR FROM: " + fromAddress.street1));
 
 const toAddress = new api.Address({
+    /*name: 'Eulaldio Perez',
+    company: 'Villas Campestres de Rancho Grande',
+    street1: 'CHAPULTEPEC 3012, MARGARITAS, 32300',
+    street2: 'Edificio rojo',
+    city: 'Ciudad Juarez',
+    state: 'Chihuahua',
+    //Colonia: 'Col. Margaritas',
+    country: 'MX',
+    zip: '32300',
+    phone: '656.616-6010',
+    email: 'tumba_burras7000@gmail.com'*/
     name: 'George Costanza',
     company: 'Vandelay Industries',
     street1: '1 E 161st St.',
@@ -23,14 +44,17 @@ const toAddress = new api.Address({
     state: 'NY',
     zip: '10451'
 });
-toAddress.save().then(console.log);
+toAddress.save().then(console.log("DIR TO: " + toAddress.street1));
+
+console.log(toAddress.state);
 
 // Step 2: Create a Parcel
 
 const parcel = new api.Parcel({
-    length: 9,
+    /*length: 9,
     width: 6,
-    height: 2,
+    height: 2,*/
+    predefined_package: 'Parcel',
     weight: 10,
 });
   
@@ -41,38 +65,19 @@ parcel.save().then(console.log);
 const shipment = new api.Shipment({
 to_address: toAddress,
 from_address: fromAddress,
-parcel: parcel
+parcel: parcel,
+carrier_accounts: ['ca_3982ca1355a641feab0cf105e7426a76'],
+mode: 'test',
 });
 
-shipment.save().then(console.log);
-
-shipment.rates.forEach(rate => {
-    console.log(rate.carrier);
-    console.log(rate.service);
-    console.log(rate.rate);
-    console.log(rate.id);
-});
+shipment.save().then(
+  console.log
+);
 
 // Step 4: Buy and Generate a Shipping Label
 
-// If you already have a saved shipment, or a shipment initialized
-// with an id:
-
-shipment.buy(shipment.lowestRate(['USPS'], ['First']))
-  .then(console.log);
-
-// or
-
-shipment.buy('{RATE_ID}').then(console.log);
-
 // If you do not have a saved shipment yet, you must save it first:
-shipment.save().then(s =>
-  s.buy(shipment.lowestRate(['USPS'], ['First']))
-    .then(console.log)
-);
-
-// Print PNG link
-console.log(shipment.postage_label.label_url);
-
-// Print Tracking Code
-console.log(shipment.tracking_code);
+/*shipment.save().then(s =>
+  s.buy(shipment.lowestRate(['DHL Express'], ['EconomySelect']))
+    //.then(console.log)
+);*/
