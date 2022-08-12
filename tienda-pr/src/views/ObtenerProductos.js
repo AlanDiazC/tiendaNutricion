253137d98356d69c522837233c6480e677a7f1e1
Alan Diaz <42380896+AlanDiazC@users.noreply.github.com>
2022-08-12 16:34:06 -0500

    Update ObtenerProductos.js

--- tienda-pr/src/views/ObtenerProductos.js
+++ tienda-pr/src/views/ObtenerProductos.js
@@ -10,22 +10,17 @@ const ObtenerProductos = ({ data, setData, setFlag }) => {
         const productsData = await getDocs(productsCollectionReference);
         //   console.log("REFERENCIA", usersCollectionReference); `clientes/${auth.currentUser.uid}/checkout_sessions`
         const prod = productsData.docs;
-
-        const precio0 = collection(db, `productos/${prod[0].id}/prices`);
-        const precio0Data = await getDocs(precio0);
-        const precio0Doc = precio0Data.docs;
-
-        var n = 2;
+        
         for (var i = 0; i < 2; i++) {
-          n += i;
+          console.log(prod[i].id);
           const precio = collection(db, `productos/${prod[i].id}/prices`);
           const precioData = await getDocs(precio);
           const precioDoc = precioData.docs;
           data[i] = {
-            id: prod[n].id,
-            nombre: prod[n].data().name,
-            imagen: prod[n].data().images[0],
-            descripcion: prod[n].data().description,
+            id: prod[i].id,
+            nombre: prod[i].data().name,
+            imagen: prod[i].data().images[0],
+            descripcion: prod[i].data().description,
             precio: precioDoc[0].data().unit_amount / 100,
             precioId: precioDoc[0].id,
             quantity: 1,
